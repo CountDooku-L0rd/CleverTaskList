e0ce1117488ad08e7a17d0d1b20c7c3700af6359
CountDookuL0rd <leon790@mail.ru>
2026-04-01 15:13:20 +0300

    ARTEMPLESHAKOV-18 [F] TL Написать логику работы приложения: Доработана логика работы приложения.

--- scripts/filterButtons.js
+++ scripts/filterButtons.js
@@ -1,5 +1,4 @@
 export const filterButtons = function (Render) {
-    let currentTabButton = document.querySelector('.filter-buttons__button-active')
     const filterButtons = document.querySelectorAll('.filter-buttons__button')
     filterButtons.forEach((filterButton) => {
         filterButton.addEventListener('click', (event) => {
@@ -7,7 +6,6 @@ export const filterButtons = function (Render) {
                 button.classList.remove('filter-buttons__button-active')
             })
             event.target.classList.add('filter-buttons__button-active')
-            currentTabButton = document.querySelector('.filter-buttons__button-active')
             Render.updateAndRender()
         })
     })
